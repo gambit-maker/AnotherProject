c11f42a571fe2b624768a79a15588d078eb025c3
HP <quanghuy12c6@gmail.com>
2020-04-15 22:29:08 +0700

    V1.5

--- app/src/main/java/vn/edu/ntu/anotherproject/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/anotherproject/MainActivity.java
@@ -4,49 +4,59 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txtTG;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("LifeCycle","onCreate");
-
     }
 
     @Override
-    protected void onStart(){
+    protected void onStart() {
         super.onStart();
         Log.d("LifeCycle","onStart");
     }
 
     @Override
-    protected void onRestart(){
+    protected void onRestart() {
         super.onRestart();
-        Log.d("LifeCyle","onRestart");
+        Log.d("LifeCycle","onRestart");
     }
 
     @Override
-    protected void onResume(){
+    protected void onResume() {
         super.onResume();
         Log.d("LifeCycle","onResume");
+        txtTG = findViewById(R.id.txtTG);
+        SimpleDateFormat sdf = new SimpleDateFormat("HH:mm:ss");
+        String date = sdf.format(new Date());
+        txtTG.setText(date);
+        Toast.makeText(MainActivity.this, "QuangHuy", Toast.LENGTH_SHORT).show();
     }
 
     @Override
-    protected void onPause(){
+    protected void onPause() {
         super.onPause();
         Log.d("LifeCycle","onPause");
     }
 
     @Override
-    protected void onStop(){
+    protected void onStop() {
         super.onStop();
         Log.d("LifeCycle","onStop");
     }
 
     @Override
-    protected void onDestroy(){
+    protected void onDestroy() {
         super.onDestroy();
         Log.d("LifeCycle","onDestroy");
     }
